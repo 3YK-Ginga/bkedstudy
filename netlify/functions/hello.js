75017c958b18f9d5a75cc8288bbb499820d98c9c
3YK-Ginga <gimai0101@gmail.com>
2024-08-22 21:31:14 +0900

    updates

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,6 +1,6 @@
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 
-const genAI = new GoogleGenerativeAI({ apiKey: process.env.GEMINI_API_KEY });
+const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
 const handler = async (event) => {
