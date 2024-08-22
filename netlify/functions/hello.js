991cb1ad41fdc514ab038b45d829033b9de04d97
3YK-Ginga <gimai0101@gmail.com>
2024-08-22 21:02:43 +0900

    Updated the file with new changes

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,13 +1,21 @@
 const axios = require('axios');
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
 
 const handler = async (event) => {
   try {
     const apiUrl = 'https://www.jalan.net/activity/json/arealist.json';
     const response = await axios.get(apiUrl);
     
+    const prompt = "Write a story about an AI and magic"
+    const result = await model.generateContent(prompt);
+    const response = await result.response;
+    const text = response.text();
+    
     return {
       statusCode: 200,
-      body: process.env.GEMINI_API_KEY,
+      body: text,
     };
   } catch (error) {
     // エラーがHTTPレスポンスに関連する場合
